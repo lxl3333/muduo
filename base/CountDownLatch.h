410883679df3f9d43c9a644f65abeb33532fd7b6
lxl3333 <2636511190@qq.com>
2023-03-13 02:43:11 +0800

    condition

---
+++ base/CountDownLatch.h
@@ -0,0 +1,35 @@
+#ifndef MUDUO_BASE_COUNTDOWNLATCH_H
+#define MUDUO_BASE_COUNTDOWNLATCH_H
+
+#include<base/Condition.h>
+#include<base/Mutex.h>
+
+namespace muduo
+{
+
+
+class CountDownLatch :noncopyable
+{
+public:
+    explicit CountDownLatch(int count);
+
+    void wait();
+
+    void countDown();
+
+    int getCount() const;
+
+
+private:
+    mutable MutexLock mutex_;
+    Condition condition_ GUARDED_BY(mutex_);
+    int count_ GUARDED_BY(mutex_);
+};
+
+
+}
+
+
+
+
+#endif
