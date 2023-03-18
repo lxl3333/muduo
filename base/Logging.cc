3b61852cb4b0860905673a4a8fd2a00b2405fcee
lxl3333 <2636511190@qq.com>
2023-03-19 01:55:13 +0800

    basedone

--- base/Logging.cc
+++ base/Logging.cc
@@ -106,3 +106,100 @@ Logger::Impl::Impl(LogLevel level ,int savedErno, const SourceFile& file ,int li
         stream_<< strcasecmp_tl(savedErno)<< "(errno =" <<savedErno<< ")";
     }
 }
+
+void Logger::Impl::formatTime()
+{
+    int64_t microSecondsSinceEpoch =time_.microSecondsSinceEpoch();
+    time_t seconds =static_cast<time_t> (microSecondsSinceEpoch/Timestamp::kMicroSecondsPerSecond);
+    int microSeconds=static_cast<int> (microSecondsSinceEpoch%Timestamp::kMicroSecondsPerSecond);
+    if(seconds !=t_lastSecond)
+    {
+        t_lastSecond =seconds;
+        struct tm tm_time;
+        if(g_logTimeZone.valid())
+        {
+            tm_time =g_logTimeZone.toLocalTime(seconds);
+        }
+        else
+        {
+            ::gmtime_r(&seconds,&tm_time);
+        }
+
+        int len = snprintf(t_time, sizeof(t_time), "%4d%02d%02d %02d:%02d:%02d",
+            tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+            tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
+        assert(len==17);(void )len;
+    }
+
+    if(g_logTimeZone.valid())
+    {
+        Fmt us(".%06d ",microSeconds);
+        assert(us.length()==8)
+        stream_<<T(t_time,17)<<T(us.data(),8);
+    }
+    else
+    {
+        Fmt us(".%06dZ ",microSeconds);
+        assert(us.length()==9);
+        stream_<< T(t_time,17)<<T(us.data(),9);
+    }
+}
+
+void Logger::Impl::finish()
+{
+    stream_<<"-" <<basename_<<':'<<line_<<'\n';
+}
+
+Logger::Logger(SourceFile file ,int line)
+:impl_(INFO,0,file,line)
+{
+
+}
+
+Logger::Logger(SourceFile file,int line, LogLevel level,const char * func)
+    :impl_(level,0,file,line)
+{
+    impl_.stream_<<func<<' ';
+}
+
+Logger::Logger(SourceFile file,int line, LogLevel level)
+    :impl_(level,0,file,line)
+{
+}
+
+Logger::Logger(SourceFile file,int line,bool toAbort)
+    :impl_(toAbort?FATAL:ERROR,errno,file,line)
+{
+}
+
+Logger::~Logger()
+{
+    impl_.finish();
+    const LogStream::Buffer& buf(stream().buffer());
+    g_output(buf.data(),buf.length());
+    if(impl_.level_==FATAL)
+    {
+        g_flush();
+        abort();
+    }
+}
+
+void Logger::setLogLevel(Logger::LogLevel level)
+{
+    g_logLevel=level;
+}
+
+void Logger::setOutput(OutputFunc out)
+{
+    g_output=out;
+}
+
+void Logger::setFlush(FlushFunc flush)
+{
+    g_flush =flush;
+}
+
+void Logger::setTimeZone(const TimeZone& tz)
+{
+    g_logTimeZone =tz;
+}
